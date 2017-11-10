35d1677305209571cd98734359fc2d889fc06551
161485W TEH HOCK TUCK <161485W@sit.nyp.edu.sg>
2017-11-10 10:18:08 +0800

    MathAdd edited

--- MathAdd.java
+++ MathAdd.java
@@ -2,7 +2,8 @@ public class MathAdd {
 	
 	public MathAdd {}
 	
-	static public int add(int a, int b) {   
+	static public int add(int a, int b) {
+	System.out.println("Sum of a and b is "+(a+b));   
         return a + b; 
 	}
 }
